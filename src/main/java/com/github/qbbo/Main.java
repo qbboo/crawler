e3ccd700b7d42cc4160381b2745ea9b8e69759f4
qbbo <qbbo@outlook.com>
2023-12-17 22:28:49 +0800

    initialization project
    add some config files
     - void test file
     - void main

---
+++ src/main/java/com/github/qbbo/Main.java
@@ -0,0 +1,7 @@
+package com.github.qbbo;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/github/qbbo/SmokeTest.java
@@ -0,0 +1,9 @@
+package com.github.qbbo;
+
+import org.junit.jupiter.api.Test;
+public class SmokeTest {
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+}
