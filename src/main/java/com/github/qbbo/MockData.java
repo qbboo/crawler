9171035a15d9bda5d9d78144e5d62fc265a3b9f2
qbbo <qbbo@outlook.com>
2023-12-22 15:57:18 +0800

    fix: findbugs error
    ICAST_INTEGER_MULTIPLY_CAST_TO_LONG and PREDICTABLE_RANDOM

--- src/main/java/com/github/qbbo/MockData.java
+++ src/main/java/com/github/qbbo/MockData.java
@@ -9,21 +9,21 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.List;
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class MockData {
     public void mockData(SqlSessionFactory sqlSessionFactory, Integer howMuch) {
         try (SqlSession session = sqlSessionFactory.openSession(ExecutorType.BATCH)) {
             List<News> newsList = session.selectList("com.github.qbbo.MockMapper.selectNews");
             int count = howMuch - newsList.size();
-            Random random = new Random();
+            SecureRandom random = new SecureRandom();
             try {
                 while (count-- > 0) {
                     System.out.println("count: " + count);
                     News news = newsList.get(random.nextInt(newsList.size()));
-                    Instant createdTime = news.getCreatedAt().minusSeconds(60 * 60 * 24 * random.nextInt(365));
+                    Instant createdTime = news.getCreatedAt().minusSeconds(60 * 60 * 24L * random.nextInt(365));
                     news.setCreatedAt(createdTime);
-                    news.setUpdatedAt(createdTime.plusSeconds(60 * 60 * 24 * random.nextInt(365)));
+                    news.setUpdatedAt(createdTime.plusSeconds(60 * 60 * 24L * random.nextInt(365)));
                     news.setTitle(news.getTitle() + random.nextInt(36090));
                     session.insert("com.github.qbbo.MockMapper.insertNews", news);
                     if (count % 2000 == 0) {
